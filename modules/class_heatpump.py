2c0e35584537bb70b2085b8311a341c285cc5920
Bla Bla <drbacke@gmx.de>
2024-03-08 14:22:11 +0100

    Wärmepumpen Klasse für meine WP erzeugt (Achtung Standard) PV Prognose aufsummieren der Strings eingebaut

--- modules/class_heatpump.py
+++ modules/class_heatpump.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 import numpy as np
 from pprint import pprint
 
-# Lade die .npz-Datei beim Start der Anwendung
+
 class Waermepumpe:
     def __init__(self, max_heizleistung, prediction_hours):
         self.max_heizleistung = max_heizleistung
@@ -21,9 +21,11 @@ class Waermepumpe:
         return heizleistung
 
     def elektrische_leistung_berechnen(self, aussentemperatur):
-        heizleistung = self.heizleistung_berechnen(aussentemperatur)
-        cop = self.cop_berechnen(aussentemperatur)
-        return heizleistung / cop
+        #heizleistung = self.heizleistung_berechnen(aussentemperatur)
+        #cop = self.cop_berechnen(aussentemperatur)
+        
+        return 1164  -77.8*aussentemperatur + 1.62*aussentemperatur**2.0
+        #1253.0*np.math.pow(aussentemperatur,-0.0682)
 
     def simulate_24h(self, temperaturen):
         leistungsdaten = []
@@ -36,6 +38,40 @@ class Waermepumpe:
             elektrische_leistung = self.elektrische_leistung_berechnen(temp)
             leistungsdaten.append(elektrische_leistung)
         return leistungsdaten
+
+# # Lade die .npz-Datei beim Start der Anwendung
+# class Waermepumpe:
+    # def __init__(self, max_heizleistung, prediction_hours):
+        # self.max_heizleistung = max_heizleistung
+        # self.prediction_hours = prediction_hours
+
+    # def cop_berechnen(self, aussentemperatur):
+        # cop = 3.0 + (aussentemperatur-0) * 0.1
+        # return max(cop, 1)
+
+
+    # def heizleistung_berechnen(self, aussentemperatur):
+        # #235.092 kWh + Temperatur * -11.645
+        # heizleistung = (((235.0) + aussentemperatur*(-11.645))*1000)/24.0
+        # heizleistung = min(self.max_heizleistung,heizleistung)
+        # return heizleistung
+
+    # def elektrische_leistung_berechnen(self, aussentemperatur):
+        # heizleistung = self.heizleistung_berechnen(aussentemperatur)
+        # cop = self.cop_berechnen(aussentemperatur)
+        # return heizleistung / cop
+
+    # def simulate_24h(self, temperaturen):
+        # leistungsdaten = []
+        
+        # # Überprüfen, ob das Temperaturarray die richtige Größe hat
+        # if len(temperaturen) != self.prediction_hours:
+            # raise ValueError("Das Temperaturarray muss genau "+str(self.prediction_hours)+" Einträge enthalten, einen für jede Stunde des Tages.")
+        
+        # for temp in temperaturen:
+            # elektrische_leistung = self.elektrische_leistung_berechnen(temp)
+            # leistungsdaten.append(elektrische_leistung)
+        # return leistungsdaten
 
 
 
