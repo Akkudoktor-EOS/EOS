8f1d23fe9d83ac94cf75a9e5a5ed410789a690c6
Bla Bla <drbacke@gmx.de>
2024-09-04 08:23:17 +0200

    E-Auto/Wallbox wird jetzt mit diskreten Ladezuständen versehen, in der config.py einstellbar Jeder DisCharge = 0 (Akkus nicht benutzen) wird mit 1Cent Strafe belegt, da die Lastverteilung fehlt. Also wenn es egal ist, soll er den Akku anschalten.

--- modules/class_optimize.py
+++ modules/class_optimize.py
@@ -23,7 +23,8 @@ from deap import base, creator, tools, algorithms
 import numpy as np
 import random
 import os
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import *
 
 def isfloat(num):
     try:
@@ -38,7 +39,7 @@ class optimization_problem:
         self.strafe = strafe
         self.opti_param = None
         self.fixed_eauto_hours = prediction_hours-optimization_hours
-
+        self.possible_charge_values = moegliche_ladestroeme_in_prozent
 
 
     def split_individual(self, individual):
@@ -74,7 +75,9 @@ class optimization_problem:
         # PARAMETER
         self.toolbox = base.Toolbox()
         self.toolbox.register("attr_bool", random.randint, 0, 1)
-        self.toolbox.register("attr_float", random.uniform, 0, 1)  # Für kontinuierliche Werte zwischen 0 und 1 (z.B. für E-Auto-Ladeleistung)
+        #self.toolbox.register("attr_float", random.uniform, 0, 1)  # Für kontinuierliche Werte zwischen 0 und 1 (z.B. für E-Auto-Ladeleistung)
+        self.toolbox.register("attr_choice", random.choice, self.possible_charge_values)  # Für diskrete Ladeströme
+
         self.toolbox.register("attr_int", random.randint, start_hour, 23)
         
         ###################
@@ -83,14 +86,14 @@ class optimization_problem:
         if opti_param["haushaltsgeraete"]>0:   
                 def create_individual():
                         attrs = [self.toolbox.attr_bool() for _ in range(self.prediction_hours)]  # 24 Bool-Werte für Entladen
-                        attrs += [self.toolbox.attr_float() for _ in range(self.prediction_hours)]  # 24 Float-Werte für Laden
+                        attrs += [self.toolbox.attr_choice()  for _ in range(self.prediction_hours)]  # 24 Float-Werte für Laden
                         attrs.append(self.toolbox.attr_int())  # Haushaltsgerät-Startzeit
                         return creator.Individual(attrs)
 
         else:
                 def create_individual():
                         attrs = [self.toolbox.attr_bool() for _ in range(self.prediction_hours)]  # 24 Bool-Werte für Entladen
-                        attrs += [self.toolbox.attr_float() for _ in range(self.prediction_hours)]  # 24 Float-Werte für Laden
+                        attrs += [self.toolbox.attr_choice()  for _ in range(self.prediction_hours)]  # 24 Float-Werte für Laden
                         return creator.Individual(attrs)
 
         
@@ -113,14 +116,14 @@ class optimization_problem:
 
 
 
-        
+        #discharge_hours_bin = np.full(self.prediction_hours,0)
         ems.set_akku_discharge_hours(discharge_hours_bin)
         
         # Setze die festen Werte für die letzten x Stunden
         for i in range(self.prediction_hours - self.fixed_eauto_hours, self.prediction_hours):
             eautocharge_hours_float[i] = 0.0  # Setze die letzten x Stunden auf einen festen Wert (oder vorgegebenen Wert)
 
-        
+        #print(eautocharge_hours_float)
         
         ems.set_eauto_charge_hours(eautocharge_hours_float)
         
@@ -141,9 +144,15 @@ class optimization_problem:
         if worst_case:
                 gesamtbilanz = gesamtbilanz * -1.0
         
+        discharge_hours_bin, eautocharge_hours_float, spuelstart_int = self.split_individual(individual)     
+        
+        # Für jeden Discharge 0, eine kleine Strafe von 1 Cent, da die Lastvertelung noch fehlt. Also wenn es egal ist, soll er den Akku entladen lassen
+        for i in range(0, self.prediction_hours):
+            if discharge_hours_bin[i] == 0.0:  # Wenn die letzten x Stunden von einem festen Wert abweichen
+                gesamtbilanz += 0.01  # Bestrafe den Optimierer
+                
         
         # E-Auto nur die ersten self.fixed_eauto_hours 
-        eautocharge_hours_float = individual[self.prediction_hours:self.prediction_hours * 2]
         for i in range(self.prediction_hours - self.fixed_eauto_hours, self.prediction_hours):
             if eautocharge_hours_float[i] != 0.0:  # Wenn die letzten x Stunden von einem festen Wert abweichen
                 gesamtbilanz += self.strafe  # Bestrafe den Optimierer
@@ -186,7 +195,7 @@ class optimization_problem:
 
     # Genetischer Algorithmus
     def optimize(self,start_solution=None):
-        population = self.toolbox.population(n=400)
+        population = self.toolbox.population(n=600)
         hof = tools.HallOfFame(1)
         
         stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -199,7 +208,7 @@ class optimization_problem:
         if start_solution is not None and start_solution != -1:
                 population.insert(0, creator.Individual(start_solution))     
         
-        algorithms.eaMuPlusLambda(population, self.toolbox, mu=200, lambda_=300, cxpb=0.3, mutpb=0.4, ngen=500,   stats=stats, halloffame=hof, verbose=True)
+        algorithms.eaMuPlusLambda(population, self.toolbox, mu=200, lambda_=300, cxpb=0.3, mutpb=0.4, ngen=300,   stats=stats, halloffame=hof, verbose=True)
         #algorithms.eaSimple(population, self.toolbox, cxpb=0.2, mutpb=0.2, ngen=1000,             stats=stats, halloffame=hof, verbose=True)
         
         member = {"bilanz":[],"verluste":[],"nebenbedingung":[]}
@@ -231,12 +240,12 @@ class optimization_problem:
         akku_size = parameter['pv_akku_cap'] # Wh
        
         einspeiseverguetung_euro_pro_wh = np.full(self.prediction_hours, parameter["einspeiseverguetung_euro_pro_wh"])  #=  # € / Wh 7/(1000.0*100.0)
-
+        discharge_array = np.full(self.prediction_hours,1) #np.array([1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0])   # 
         akku = PVAkku(kapazitaet_wh=akku_size,hours=self.prediction_hours,start_soc_prozent=parameter["pv_soc"], max_ladeleistung_w=5000)
-        discharge_array = np.full(self.prediction_hours,1) #np.array([1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0])   #
+        akku.set_charge_per_hour(discharge_array)
+        
+        
         laden_moeglich = np.full(self.prediction_hours,1) # np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0])
-        
-        
         eauto = PVAkku(kapazitaet_wh=parameter["eauto_cap"], hours=self.prediction_hours, lade_effizienz=parameter["eauto_charge_efficiency"], entlade_effizienz=1.0, max_ladeleistung_w=parameter["eauto_charge_power"] ,start_soc_prozent=parameter["eauto_soc"])
         eauto.set_charge_per_hour(laden_moeglich)
         min_soc_eauto = parameter['eauto_min_soc']
