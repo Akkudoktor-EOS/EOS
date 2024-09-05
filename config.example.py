f951605c2d8b603ac4a0af3224586214e3a425e4
Bla Bla <drbacke@gmx.de>
2024-09-05 13:56:46 +0200

    optimzation_hours

--- config.example.py
+++ config.example.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 
 prediction_hours=48 
+optimization_hours=24
 strafe=10
 moegliche_ladestroeme_in_prozent = [0.0 ,6.0/16.0, 7.0/16.0, 8.0/16.0, 9.0/16.0, 10.0/16.0, 11.0/16.0, 12.0/16.0, 13.0/16.0, 14.0/16.0, 15.0/16.0, 1.0 ]
 
