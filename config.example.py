42093682e1d1a70a11f54460e5a40f5b0892cfc0
NormannK <github@koldrack.com>
2024-09-21 14:01:22 +0200

    Update config.example.py
    simplifications

--- config.example.py
+++ config.example.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta
 
+# Configuration
+prediction_hours = 48
+optimization_hours = 24
+strafe = 10
 
-prediction_hours=48
-optimization_hours=24
-strafe=10
-moegliche_ladestroeme_in_prozent = [0.0 ,6.0/16.0, 7.0/16.0, 8.0/16.0, 9.0/16.0, 10.0/16.0, 11.0/16.0, 12.0/16.0, 13.0/16.0, 14.0/16.0, 15.0/16.0, 1.0 ]
+# Lade Ströme in Prozent (possible charge currents in percentage)
+moegliche_ladestroeme_in_prozent = [i / 16.0 for i in range(6, 17)] # [0.0 ,6.0/16.0, 7.0/16.0, 8.0/16.0, 9.0/16.0, 10.0/16.0, 11.0/16.0, 12.0/16.0, 13.0/16.0, 14.0/16.0, 15.0/16.0, 1.0 ]
 
-
-# Optional
+# Optional Database Configuration
 db_config = {
     'user': 'eos',
     'password': 'eos',
@@ -15,14 +16,11 @@ db_config = {
     'database': 'eos'
 }
 
-def get_start_enddate(prediction_hours=48,startdate=None):
-        ############
-        # Parameter
-        ############
-        if startdate == None:
-                date = (datetime.now().date() + timedelta(hours = prediction_hours)).strftime("%Y-%m-%d")
-                date_now = datetime.now().strftime("%Y-%m-%d")
-        else:
-                date = (startdate + timedelta(hours = prediction_hours)).strftime("%Y-%m-%d")
-                date_now = startdate.strftime("%Y-%m-%d")
-        return date_now,date
+def get_start_enddate(prediction_hours=48, startdate=None):
+    if startdate is None:
+        startdate = datetime.now()
+
+    date_now = startdate.strftime("%Y-%m-%d")
+    end_date = (startdate + timedelta(hours=prediction_hours)).strftime("%Y-%m-%d")
+
+    return date_now, end_date
