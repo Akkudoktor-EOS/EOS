348cd735fafd246d43e4992d4f7de136391ce2b5
Michael Osthege <michael.osthege@outlook.com>
2024-10-06 01:00:56 +0200

    Remove conftest file

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from modules.heatpump import Heatpump
-
-
-@pytest.fixture(scope="function")
-def hp_5kw_24h() -> Heatpump:
-    """Heat pump with 5 kw heating output and 24 h prediction"""
-    return Heatpump(5000, 24)
