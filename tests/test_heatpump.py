c6041172265914a9032f2c590e18fa0662eb7429
Bobby Noelte <b0661n0e17e@gmail.com>
2024-10-11 10:55:47 +0200

    Check Google style source commenting.
    Check Google style commenting by the appropriate ruff rules.
    
    Commenting is _NOT_ enforced. Missing docstrings are ignored.
    
    Minor commenting quirks of the code base are adapted.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- tests/test_heatpump.py
+++ tests/test_heatpump.py
@@ -5,13 +5,13 @@ from akkudoktoreos.heatpump import Heatpump
 
 @pytest.fixture(scope="function")
 def hp_5kw_24h() -> Heatpump:
-    """Heatpump with 5 kw heating power and 24 h prediction"""
+    """Heatpump with 5 kw heating power and 24 h prediction."""
     return Heatpump(5000, 24)
 
 
 class TestHeatpump:
     def test_cop(self, hp_5kw_24h: Heatpump):
-        """Testing calculate COP for variouse outside temperatures"""
+        """Testing calculate COP for variouse outside temperatures."""
         assert hp_5kw_24h.calculate_cop(-10) == 2.0
         assert hp_5kw_24h.calculate_cop(0) == 3.0
         assert hp_5kw_24h.calculate_cop(10) == 4.0
@@ -24,13 +24,13 @@ class TestHeatpump:
             hp_5kw_24h.calculate_cop(out_temp_max)
 
     def test_heating_output(self, hp_5kw_24h: Heatpump):
-        """Testing calculate of heating output"""
+        """Testing calculate of heating output."""
         assert hp_5kw_24h.calculate_heating_output(-10.0) == 5000
         assert hp_5kw_24h.calculate_heating_output(0.0) == 5000
         assert hp_5kw_24h.calculate_heating_output(10.0) == pytest.approx(4939.583)
 
     def test_heating_power(self, hp_5kw_24h: Heatpump):
-        """Testing calculation of heating power"""
+        """Testing calculation of heating power."""
         assert hp_5kw_24h.calculate_heat_power(-10.0) == 2104
         assert hp_5kw_24h.calculate_heat_power(0.0) == 1164
         assert hp_5kw_24h.calculate_heat_power(10.0) == 548
