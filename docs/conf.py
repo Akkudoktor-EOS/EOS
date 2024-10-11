a1cef1e965bc01e33325a5301baf86c39abd8d84
Bobby Noelte <b0661n0e17e@gmail.com>
2024-10-11 10:54:15 +0200

    Enable Google style source commenting and documentation generation.
    Enable automatic documentation generation from Google style docstrings in the source.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 import sys
 from pathlib import Path
@@ -20,6 +21,7 @@ release = "0.0.1"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
     "myst_parser",
     "sphinxcontrib.openapi",
@@ -64,3 +66,19 @@ autosummary_generate = True
 
 # -- Options for openapi -----------------------------------------------------
 openapi_default_renderer = "httpdomain:old"
+
+# -- Options for napoleon -------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
