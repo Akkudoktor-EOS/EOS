602f4d30f1116befde9e9c080776cbd8edae7ffa
Dominique Lasserre <lasserre.d@gmail.com>
2024-10-14 00:12:00 +0200

    Add more optimization tests.
     * Include test run from single_test_optimization into pytest run.
     * Check for complete result with pytest.approx (1e-6 tolerance) to spot
       changes to expected result quickly.
     * Add one long test run (skipped by default).
     * Use --full-run pytest option to activate long test run (400 gen like
       single_test_optimization).

--- tests/test_class_optimize.py
+++ tests/test_class_optimize.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Any
 
 import pytest
 from mock import patch
@@ -9,9 +10,30 @@ from akkudoktoreos.class_optimize import optimization_problem
 DIR_TESTDATA = Path(__file__).parent / "testdata"
 
 
-@pytest.mark.parametrize("fn_in, fn_out", [("optimize_input_1.json", "optimize_result_1.json")])
+def compare_dict(actual: dict[str, Any], expected: dict[str, Any]):
+    assert set(actual) == set(expected)
+
+    for key, value in expected.items():
+        if isinstance(value, dict):
+            assert isinstance(actual[key], dict)
+            compare_dict(actual[key], value)
+        elif isinstance(value, list):
+            assert isinstance(actual[key], list)
+            assert actual[key] == pytest.approx(value)
+        else:
+            assert actual[key] == pytest.approx(value)
+
+
+@pytest.mark.parametrize(
+    "fn_in, fn_out, ngen",
+    [
+        ("optimize_input_1.json", "optimize_result_1.json", 3),
+        ("optimize_input_2.json", "optimize_result_2.json", 3),
+        ("optimize_input_2.json", "optimize_result_2_full.json", 400),
+    ],
+)
 @patch("akkudoktoreos.class_optimize.visualisiere_ergebnisse")
-def test_optimize(visualisiere_ergebnisse_patch, fn_in, fn_out):
+def test_optimize(visualisiere_ergebnisse_patch, fn_in: str, fn_out: str, ngen: int, is_full_run):
     # Load input and output data
     with open(DIR_TESTDATA / fn_in, "r") as f_in:
         input_data = json.load(f_in)
@@ -24,15 +46,20 @@ def test_optimize(visualisiere_ergebnisse_patch, fn_in, fn_out):
     )
     start_hour = 10
 
+    if ngen > 10 and not is_full_run:
+        pytest.skip()
+
     # Call the optimization function
-    ergebnis = opt_class.optimierung_ems(parameter=input_data, start_hour=start_hour, ngen=3)
-    # with open("new.json", "w") as f_out:
-    #     json.dump(ergebnis, f_out, indent=4)
+    ergebnis = opt_class.optimierung_ems(parameter=input_data, start_hour=start_hour, ngen=ngen)
+    # with open(f"new_{fn_out}", "w") as f_out:
+    #     from akkudoktoreos.class_numpy_encoder import NumpyEncoder
+    #     json_data_str = NumpyEncoder.dumps(ergebnis)
+    #     json.dump(json.loads(json_data_str), f_out, indent=4)
 
     # Assert that the output contains all expected entries.
     # This does not assert that the optimization always gives the same result!
     # Reproducibility and mathematical accuracy should be tested on the level of individual components.
-    assert set(ergebnis) == set(expected_output_data)
+    compare_dict(ergebnis, expected_output_data)
 
     # The function creates a visualization result PDF as a side-effect.
     visualisiere_ergebnisse_patch.assert_called_once()
