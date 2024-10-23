265f3618cecc93e776a5fa9638008a2e90372c16
Normann <github@koldrack.com>
2024-10-23 18:36:12 +0200

    revert to_dict name change

--- src/akkudoktoreos/class_akku.py
+++ src/akkudoktoreos/class_akku.py
@@ -31,7 +31,7 @@ class PVAkku:
         self.min_soc_wh = (self.min_soc_prozent / 100) * self.kapazitaet_wh
         self.max_soc_wh = (self.max_soc_prozent / 100) * self.kapazitaet_wh
 
-    def return_dict(self):
+    def to_dict(self):
         return {
             "kapazitaet_wh": self.kapazitaet_wh,
             "start_soc_prozent": self.start_soc_prozent,
--- src/akkudoktoreos/class_optimize.py
+++ src/akkudoktoreos/class_optimize.py
@@ -344,7 +344,7 @@ class optimization_problem:
             "discharge_hours_bin": discharge_hours_bin,
             "eautocharge_hours_float": eautocharge_hours_float,
             "result": o,
-            "eauto_obj": ems.eauto.return_dict(),
+            "eauto_obj": ems.eauto.to_dict(),
             "start_solution": start_solution,
             "spuelstart": spuelstart_int,
             "simulation_data": o,
