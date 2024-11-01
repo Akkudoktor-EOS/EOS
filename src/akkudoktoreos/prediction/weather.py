afc2414a14785ae147295fc863082e3b92b23284
Bobby Noelte <b0661n0e17e@gmail.com>
2024-11-01 10:00:22 +0100

    Add weather forecast to prediction submodule.
    The modules provide classes and methods to retrieve, manage, and process weather forecast data
    from various sources. Includes are structured representations of weather data and utilities
    for fetching forecasts for specific locations and time ranges.
    
    BrightSky and ClearOutside are currently supported.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

---
+++ src/akkudoktoreos/prediction/weather.py
@@ -0,0 +1,13 @@
+"""Weather forecast module for weather predictions."""
+
+from typing import Optional
+
+from pydantic import Field
+
+from akkudoktoreos.config.configabc import SettingsBaseModel
+
+
+class WeatherCommonSettings(SettingsBaseModel):
+    weather_provider: Optional[str] = Field(
+        default="ClearOutside", description="Weather provider id of provider to be used."
+    )
