45216dbadb71eca7bfcaa116fad927b8166fc324
Fortrieb <dev@fortrieb.org>
2024-11-11 22:16:53 +0100

    Add doc strings and translation

--- src/akkudoktoreos/battery.py
+++ src/akkudoktoreos/battery.py
@@ -1,11 +1,13 @@
+from typing import Tuple
+
 import numpy as np
 
 
 class Battery:
     def __init__(
         self,
-        capacity_wh: int = None,
-        hours: int = None,
+        capacity_wh: int = 0,
+        hours: int = 0,
         charge_efficiency: float = 0.88,
         discharge_efficiency: float = 0.88,
         max_charging_power_w: float = None,
@@ -13,9 +15,19 @@ class Battery:
         min_soc_percent: int = 0,
         max_soc_percent: int = 100,
     ):
-        # Battery capacity in Wh
+        """Base class for an energy storage battery.
+
+        Args:
+            capacity_wh (optional): Battery capacity in Watt hours. Defaults to 0.
+            hours (optional): Amount of hours battery is simulated. Defaults to 0.
+            charge_efficiency (optional): Value between 0 to 1.0. Defaults to 0.88.
+            discharge_efficiency (optional): Value between 0 to 1.0. Defaults to 0.88.
+            max_charging_power_w (optional): Power to charge battery in Watt. Defaults to None.
+            start_soc_percent (optional): State of charge (SoC) in percent. Defaults to 0.
+            min_soc_percent (optional): Minimal SoC in percent. Defaults to 0.
+            max_soc_percent (optional): Maximum SoC in percent. Defaults to 100.
+        """
         self.capacity_wh = capacity_wh
-        # Initial state of charge in Wh
         self.start_soc_percent = start_soc_percent
         self.soc_wh = (start_soc_percent / 100) * capacity_wh
         self.hours = (
@@ -54,26 +66,35 @@ class Battery:
         }
 
     @classmethod
-    def from_dict(cls, data):
-        # Create a new object with basic data
+    def from_dict(cls, data: dict):
+        """Create a Battery object from dictionary.
+
+        Args:
+            data: Dictionary containing a single battery object.
+
+        Returns:
+            _type_: _description_
+        """
         obj = cls(
-            data["kapazitaet_wh"],
-            data["hours"],
-            data["lade_effizienz"],
-            data["entlade_effizienz"],
-            data["max_ladeleistung_w"],
-            data["start_soc_prozent"],
+            data.get("capacity_wh", 0),
+            data.get("hours", 0),
+            data.get("charging_efficiency"),
+            data.get("discharge_efficiency"),
+            data.get("start_soc_percent", 0),
+            data.get("max_charge_power_w"),
+            data.get("min_soc_percent", 0),
+            data.get("max_soc_percent", 100),
         )
         # Set arrays
         obj.discharge_array = np.array(data["discharge_array"])
         obj.charge_array = np.array(data["charge_array"])
-        obj.soc_wh = data[
+        obj.soc_wh = data.get(
             "soc_wh"
-        ]  # Set current state of charge, which may differ from start_soc_prozent
-
+        )  # Set current state of charge, which may differ from start_soc_prozent
         return obj
 
     def reset(self):
+        """Reset battery fields 'soc_wh', 'discharge_array' and 'charge_array'"""
         self.soc_wh = (self.start_soc_percent / 100) * self.capacity_wh
         # Ensure soc_wh is within min and max limits
         self.soc_wh = min(max(self.soc_wh, self.min_soc_wh), self.max_soc_wh)
@@ -89,10 +110,19 @@ class Battery:
         assert len(charge_array) == self.hours
         self.charge_array = np.array(charge_array)
 
-    def charge_state_percent(self):
+    def charge_state_percent(self) -> float:
         return (self.soc_wh / self.capacity_wh) * 100
 
-    def discharge(self, wh, hour):
+    def discharge(self, wh: int, hour: int) -> Tuple[float, float]:
+        """Discharge battery with provided watt hours and duration in hours
+
+        Args:
+            wh: Watt hours to discharge
+            hour: Time of discharge
+
+        Returns:
+            Tuple[float, float]: actual released watt hours, loss watt hours
+        """
         if self.discharge_array[hour - 1] == 0:
             return 0.0, 0.0  # No energy discharge and no losses
 
@@ -105,31 +135,38 @@ class Battery:
         )  # Ensure non-negative
 
         # Consider the maximum discharge power of the battery
-        max_abgebbar_wh = min(max_possible_discharge_wh, self.max_charging_power_w)
+        max_released_wh = min(max_possible_discharge_wh, self.max_charging_power_w)
 
         # The actually discharged energy cannot exceed requested energy or maximum discharge
-        tatsaechlich_abgegeben_wh = min(wh, max_abgebbar_wh)
+        actual_released_wh = min(wh, max_released_wh)
 
         # Calculate the actual amount withdrawn from the battery (before efficiency loss)
         if self.discharge_efficiency > 0:
-            tatsaechliche_entnahme_wh = (
-                tatsaechlich_abgegeben_wh / self.discharge_efficiency
-            )
+            actual_withdrawal_wh = actual_released_wh / self.discharge_efficiency
         else:
-            tatsaechliche_entnahme_wh = 0.0
+            actual_withdrawal_wh = 0.0
 
         # Update the state of charge considering the actual withdrawal
-        self.soc_wh -= tatsaechliche_entnahme_wh
+        self.soc_wh -= actual_withdrawal_wh
         # Ensure soc_wh does not go below min_soc_wh
         self.soc_wh = max(self.soc_wh, self.min_soc_wh)
 
         # Calculate losses due to efficiency
-        verluste_wh = tatsaechliche_entnahme_wh - tatsaechlich_abgegeben_wh
+        loss_wh = actual_withdrawal_wh - actual_released_wh
 
         # Return the actually discharged energy and the losses
-        return tatsaechlich_abgegeben_wh, verluste_wh
+        return actual_released_wh, loss_wh
 
-    def charge(self, wh: int, hour: int):
+    def charge(self, wh: int, hour: int) -> Tuple[float, float]:
+        """Charge battery with provided watt hours and time in hours.
+
+        Args:
+            wh (int): Watt hours to charge
+            hour (int): Time to charge
+
+        Returns:
+            Tuple[float, float]: Charged energy, loss watt hours
+        """
         if hour and self.charge_array[hour - 1] == 0:
             return 0, 0  # Charging not allowed in this hour
 
@@ -137,8 +174,8 @@ class Battery:
         wh = wh if wh is not None else self.max_charging_power_w
 
         # Relative to the maximum charging power (between 0 and 1)
-        relative_ladeleistung = self.charge_array[hour - 1]
-        effektive_ladeleistung = relative_ladeleistung * self.max_charging_power_w
+        relative_charge_power = self.charge_array[hour - 1]
+        effective_charge_power = relative_charge_power * self.max_charging_power_w
 
         # Calculate the maximum energy that can be charged considering max_soc and efficiency
         if self.charge_efficiency > 0:
@@ -150,22 +187,22 @@ class Battery:
         max_possible_charge_wh = max(max_possible_charge_wh, 0.0)  # Ensure non-negative
 
         # The actually charged energy cannot exceed requested energy, charging power, or maximum possible charge
-        effektive_lademenge = min(wh, effektive_ladeleistung, max_possible_charge_wh)
+        effective_charge = min(wh, effective_charge_power, max_possible_charge_wh)
 
         # Energy actually stored in the battery
-        geladene_menge = effektive_lademenge * self.charge_efficiency
+        charged_energy = effective_charge * self.charge_efficiency
 
         # Update soc_wh
-        self.soc_wh += geladene_menge
+        self.soc_wh += charged_energy
         # Ensure soc_wh does not exceed max_soc_wh
         self.soc_wh = min(self.soc_wh, self.max_soc_wh)
 
         # Calculate losses
-        verluste_wh = effektive_lademenge - geladene_menge
+        loss_wh = effective_charge - charged_energy
 
-        return geladene_menge, verluste_wh
+        return charged_energy, loss_wh
 
-    def current_energy(self):
+    def current_energy(self) -> float:
         """
         This method returns the current remaining energy considering efficiency.
         It accounts for both charging and discharging efficiency.
