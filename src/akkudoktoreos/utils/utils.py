6abb0cac0fb146f72bac68018e69540944c475d5
Bobby Noelte <b0661n0e17e@gmail.com>
2024-11-14 13:56:05 +0100

    Add optimization base configuration modules.
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- src/akkudoktoreos/utils/utils.py
+++ src/akkudoktoreos/utils/utils.py
@@ -1,23 +1,7 @@
-import datetime
 import json
-import zoneinfo
 from typing import Any
 
 import numpy as np
-
-
-# currently unused
-def ist_dst_wechsel(tag: datetime.datetime, timezone: str = "Europe/Berlin") -> bool:
-    """Checks if Daylight Saving Time (DST) starts or ends on a given day."""
-    tz = zoneinfo.ZoneInfo(timezone)
-    # Get the current day and the next day
-    current_day = datetime.datetime(tag.year, tag.month, tag.day)
-    next_day = current_day + datetime.timedelta(days=1)
-
-    # Check if the UTC offsets are different (indicating a DST change)
-    dst_change = current_day.replace(tzinfo=tz).dst() != next_day.replace(tzinfo=tz).dst()
-
-    return dst_change
 
 
 class NumpyEncoder(json.JSONEncoder):
