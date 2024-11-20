db789afe79b0c1d143e242160ab72ab3a057c720
Bobby Noelte <b0661n0e17e@gmail.com>
2024-11-20 14:31:08 +0100

    Adapt visualize to config.
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 from matplotlib.testing.compare import compare_images
 
-from akkudoktoreos.config import AppConfig
+from akkudoktoreos.config.config import get_config
 from akkudoktoreos.visualize import visualisiere_ergebnisse
 
 DIR_TESTDATA = Path(__file__).parent / "testdata"
@@ -15,11 +15,15 @@ DIR_IMAGEDATA = DIR_TESTDATA / "images"
     "fn_in, fn_out, fn_out_base",
     [("visualize_input_1.json", "visualize_output_1.pdf", "visualize_base_output_1.pdf")],
 )
-def test_visualisiere_ergebnisse(fn_in, fn_out, fn_out_base, tmp_config: AppConfig):
+def test_visualisiere_ergebnisse(fn_in, fn_out, fn_out_base):
     with open(DIR_TESTDATA / fn_in, "r") as f:
         input_data = json.load(f)
-    visualisiere_ergebnisse(config=tmp_config, **input_data)
-    output_file: Path = tmp_config.working_dir / tmp_config.directories.output / fn_out
+    visualisiere_ergebnisse(**input_data)
+
+    config = get_config()
+    output_dir = config.data_output_path
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_file = output_dir.joinpath(fn_out)
 
     assert output_file.is_file()
     assert (
