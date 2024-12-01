d3ba26afac5ffbb06ad853048718a83daf0ba64a
Bobby Noelte <b0661n0e17e@gmail.com>
2024-12-01 21:10:01 +0100

    Adapt devices to config.
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- src/akkudoktoreos/devices/generic.py
+++ src/akkudoktoreos/devices/generic.py
@@ -1,5 +1,12 @@
+from typing import Optional
+
 import numpy as np
 from pydantic import BaseModel, Field
+
+from akkudoktoreos.devices.devicesabc import DeviceBase
+from akkudoktoreos.utils.logutil import get_logger
+
+logger = get_logger(__name__)
 
 
 class HomeApplianceParameters(BaseModel):
@@ -13,21 +20,57 @@ class HomeApplianceParameters(BaseModel):
     )
 
 
-class HomeAppliance:
-    def __init__(self, parameters: HomeApplianceParameters, hours: int = 24):
-        self.hours = hours  # Total duration for which the planning is done
-        self.consumption_wh = (
-            parameters.consumption_wh
-        )  # Total energy consumption of the device in kWh
-        self.duration_h = parameters.duration_h  # Duration of use in hours
+class HomeAppliance(DeviceBase):
+    def __init__(
+        self,
+        parameters: Optional[HomeApplianceParameters] = None,
+        hours: Optional[int] = 24,
+        provider_id: Optional[str] = None,
+    ):
+        # Configuration initialisation
+        self.provider_id = provider_id
+        self.prefix = "<invalid>"
+        if self.provider_id == "GenericDishWasher":
+            self.prefix = "dishwasher"
+        # Parameter initialisiation
+        self.parameters = parameters
+        if hours is None:
+            self.hours = self.total_hours
+        else:
+            self.hours = hours
+
+        self.initialised = False
+        # Run setup if parameters are given, otherwise setup() has to be called later when the config is initialised.
+        if self.parameters is not None:
+            self.setup()
+
+    def setup(self) -> None:
+        if self.initialised:
+            return
+        if self.provider_id is not None:
+            # Setup by configuration
+            self.hours = self.total_hours
+            self.consumption_wh = getattr(self.config, f"{self.prefix}_consumption")
+            self.duration_h = getattr(self.config, f"{self.prefix}_duration")
+        elif self.parameters is not None:
+            # Setup by parameters
+            self.consumption_wh = (
+                self.parameters.consumption_wh
+            )  # Total energy consumption of the device in kWh
+            self.duration_h = self.parameters.duration_h  # Duration of use in hours
+        else:
+            error_msg = "Parameters and provider ID missing. Can't instantiate."
+            logger.error(error_msg)
+            raise ValueError(error_msg)
         self.load_curve = np.zeros(self.hours)  # Initialize the load curve with zeros
+        self.initialised = True
 
     def set_starting_time(self, start_hour: int, global_start_hour: int = 0) -> None:
         """Sets the start time of the device and generates the corresponding load curve.
 
         :param start_hour: The hour at which the device should start.
         """
-        self.reset()
+        self.reset_load_curve()
         # Check if the duration of use is within the available time frame
         if start_hour + self.duration_h > self.hours:
             raise ValueError("The duration of use exceeds the available time frame.")
@@ -40,7 +83,7 @@ class HomeAppliance:
         # Set the power for the duration of use in the load curve array
         self.load_curve[start_hour : start_hour + self.duration_h] = power_per_hour
 
-    def reset(self) -> None:
+    def reset_load_curve(self) -> None:
         """Resets the load curve."""
         self.load_curve = np.zeros(self.hours)
 
