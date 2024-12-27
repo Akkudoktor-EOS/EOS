19fc3cf82ae721f63c9f896e74a28dcb021094a6
Stefan Ortgies <stefan@flensburg.dev>
2024-12-27 01:25:41 +0100

    fixed test that failed after commit "Translated fastAPI endpoints"

--- tests/test_server.py
+++ tests/test_server.py
@@ -17,7 +17,7 @@ def test_fixture_setup(server, tmp_path: Path) -> None:
 
 def test_server(server, tmp_path: Path):
     """Test the server."""
-    result = requests.get(f"{server}/gesamtlast_simple?year_energy=2000&")
+    result = requests.get(f"{server}/load_total_simple?year_energy=2000")
     assert result.status_code == HTTPStatus.OK
 
     config = load_config(tmp_path, False)
