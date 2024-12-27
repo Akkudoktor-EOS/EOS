75cb5d44a2db89054c799c172a01afb0f47989ba
Stefan Ortgies <stefan@flensburg.dev>
2024-12-27 01:22:34 +0100

    According to pydantic documentation @field_validator needs @classmethod (https://docs.pydantic.dev/2.10/concepts/validators/#field-validators)
    (cherry picked from commit a1a936b94bb8fc27460cd76ab770c34e47236ce1)

--- src/akkudoktoreos/optimization/genetic.py
+++ src/akkudoktoreos/optimization/genetic.py
@@ -47,6 +47,7 @@ class OptimizationParameters(BaseModel):
             raise ValueError("Input lists have different lengths")
         return self
 
+    @classmethod
     @field_validator("start_solution")
     def validate_start_solution(
         cls, start_solution: Optional[list[float]]
@@ -80,6 +81,7 @@ class OptimizationResponse(BaseModel):
         description="Can be `null` or contain an object representing the start of washing (if applicable).",
     )
 
+    @classmethod
     @field_validator(
         "ac_charge",
         "dc_charge",
@@ -89,6 +91,7 @@ class OptimizationResponse(BaseModel):
     def convert_numpy(cls, field: Any) -> Any:
         return NumpyEncoder.convert_numpy(field)[0]
 
+    @classmethod
     @field_validator(
         "eauto_obj",
         mode="before",
